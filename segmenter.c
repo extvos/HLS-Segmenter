2e813417402fdff61c3b74554ce2fc0e4b7970e1
Stoian Ivanov <sdr@tera-com.com>
2014-01-15 18:39:01 +0200

    more live friendly

--- segmenter.c
+++ segmenter.c
@@ -119,7 +119,7 @@ int write_index_file(
 
 
 
-	fprintf(tmp_index_fp,  "#EXTM3U\n#EXT-X-TARGETDURATION:%u\n", maxDuration);
+	fprintf(tmp_index_fp,  "#EXTM3U\n#EXT-X-MEDIA-SEQUENCE:%u\n#EXT-X-TARGETDURATION:%u\n", maxDuration,segment_number_offset);
 	
 
 	for (i = 0; i <numsegments; i++) {
